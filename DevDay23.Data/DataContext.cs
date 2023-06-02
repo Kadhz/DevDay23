daf3e093c422e41bf284edb4742c29069e0e8fc2
Kadhz <carloskadu.silva123@gmail.com>
2023-06-02 16:33:19 -0300

    Add project files.

---
+++ DevDay23.Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DevDay23.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay23.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Aluno> ALUNO { get; set; }
+    }
+}
---
+++ DevDay23.Domain/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay23.Domain
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+
+        public char Name { get; set; }
+
+        public string RA { get; set; }
+
+        public int Age { get; set; }
+
+
+
+
+    }
+}
